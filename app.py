6d4acff24e063fe0ef75dd79785abbaf9e5f05f2
Amirhossein Ghadiri <amirhosseinghadiri97@gmail.com>
2022-08-30 01:08:19 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1177,13 +1177,13 @@ st.text('Fuzzy functions imported.')
   
 
 def PSNR(y_true, y_pred):
-mse = np.mean((y_true - y_pred) ** 2)
-if(mse == 0):  # MSE is zero means no noise is present in the signal .
-              # Therefore PSNR have no importance.
-    return 100
-max_pixel = 255.0
-psnr = 20 * log10(max_pixel / sqrt(mse))
-return psnr
+    mse = np.mean((y_true - y_pred) ** 2)
+    if(mse == 0):  # MSE is zero means no noise is present in the signal .
+                  # Therefore PSNR have no importance.
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse))
+    return psnr
     
     
     
